30a7451aa9387acd7e73dc362331e617b6f5efaf
Peter Collins <peter@drifty.com>
2013-10-09 19:40:39 -0500

    Fix social settings to not require email on signup

--- followbot/config/settings.py
+++ followbot/config/settings.py
@@ -224,8 +224,8 @@ class Common(Configuration):
 
     # Some really nice defaults
     ACCOUNT_AUTHENTICATION_METHOD = "username"
-    ACCOUNT_EMAIL_REQUIRED = True
-    ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+    #ACCOUNT_EMAIL_REQUIRED = True
+    #ACCOUNT_EMAIL_VERIFICATION = "mandatory"
     ########## END AUTHENTICATION CONFIGURATION
 
     ########## Custom user app defaults
