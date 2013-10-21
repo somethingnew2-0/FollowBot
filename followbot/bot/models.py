7733a2826905c22e929523ff2ef1969c1c68857e
Peter Collins <peter@drifty.com>
2013-10-21 13:09:40 -0500

    Add initial keyword model

--- followbot/bot/models.py
+++ followbot/bot/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Keyword(models.Model):
+
+    keyword = models.TextField(blank=False, null=False)
+    user = models.ForeignKey(User, null=False, blank=False)
+
+    def __unicode__(self):
+        return self.keyword
