6edf8701c7854f48882ce88216a7d4b3cd2b6960
Peter Collins <peter@drifty.com>
2013-10-21 21:09:49 -0500

    Comment out bid for now

--- followbot/bot/models.py
+++ followbot/bot/models.py
@@ -1,22 +1,37 @@
 from django.db import models
+from decimal import Decimal
 from users.models import User, Follower
 
-# Keywords that the user has selected
+# Users custom search term
+class SearchTerm(models.Model):
+	# Formatted to how the user wanted
+    term = models.CharField(max_length=140)
+    user = models.ForeignKey(User)
+    deleted = models.BooleanField(default=False)
+
+    #bid = models.DecimalField(max_digits=9, decimal_places=2, default=Decimal('0.00'))
+
+    def __unicode__(self):
+        return self.keyword
+
+# Unified lowercase keywords that the user 
+# has selected which are in a search term
 class Keyword(models.Model):
     keyword = models.CharField(max_length=140)
-    user = models.ForeignKey(User)
+    term = models.ForeignKey(SearchTerm)
 
+    #bid = models.DecimalField(max_digits=9, decimal_places=2, default=Decimal('0.00'))
     def __unicode__(self):
         return self.keyword
 
 # Log for favorites done
 class Favorite(models.Model):
-    keyword = models.ForeignKey(Keyword)
+    keyword = models.ForeignKey(SearchTerm)
     user = models.ForeignKey(User)	
     favoritedTime =  models.DateTimeField(auto_now_add=True)
 
     # Was the favorite deleted, not the tweet
-    deleted = models.BooleanField()
+    deleted = models.BooleanField(default=False)
 
     tweetId = models.BigIntegerField()
     tweetTime = models.DateTimeField()
