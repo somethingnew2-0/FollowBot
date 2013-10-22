13ca8967df9c403a9d12db7aa9166788eddb3626
Peter Collins <peter@drifty.com>
2013-10-21 21:18:20 -0500

    Fix imports for tasks code

--- followbot/bot/tasks.py
+++ followbot/bot/tasks.py
@@ -1,10 +1,10 @@
 from celery import task
-from users.models import User, Follower, DeltaFollower
-from bot.models import Keyword, Tweet
+from users.models import User, Follower
+from bot.models import Query, Favorite, DeltaFollower
 from twitter import Twitter, OAuth, TwitterHTTPError
 
 @task()
 def favorite():   
-	user.socialaccount_set.get(provider='twitter').extra_data
+    #user.socialaccount_set.get(provider='twitter').extra_data
 
     print "favorite"
